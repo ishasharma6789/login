18d7029ecd238521fb639cbc0f7363317a0bdf5e
Isha Rani Sharma <94365531+ishasharma6789@users.noreply.github.com>
2023-09-26 02:26:31 +0530

    login/

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Register from './Register';
+import Login from "./login";
+
+
+function App() {
+
+  return (
+    <main className="App">
+      <Login />
+    </main>
+  );
+}
+
+export default App;
