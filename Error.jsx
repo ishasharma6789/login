915a618500370c253066db7c6cbf093d0a16f1c0
Samxpi <samprit_202100020@smit.smu.edu.in>
2023-09-26 11:03:21 +0530

    Fixed folders

--- Error.jsx
+++
@@ -1,4 +0,0 @@
-const Error = () => {
-    return <h1> Oops,Page Not Found </h1>
-};
-export default Error;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { AuthProvider } from './context/AuthProvider';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
